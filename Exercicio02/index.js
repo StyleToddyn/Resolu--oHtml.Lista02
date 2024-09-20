1890cdbd6ae5edba56124944ee67292f31014209
Vitr Diógenes <vitor.diogens@gmail.com>
2024-09-20 16:36:28 -0300

    Correção da questão 02

--- Exercicio02/index.js
+++ Exercicio02/index.js
@@ -3,7 +3,7 @@ const v2 = document.querySelector('#valor2');
 const resultado = document.querySelector('#resultado');
 const botaoCalcular = document.querySelector('#calcular');
 
-botaoCalcular.onclick = function(v1,v2){
+botaoCalcular.onclick = function(){
     let soma = Number.parseInt(v1.value) + Number.parseInt(v2.value);
     resultado.value = soma;
 };
