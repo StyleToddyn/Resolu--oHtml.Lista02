d732459275e458340c1523dbd2805b72b155c55b
Vitr Diógenes <vitor.diogens@gmail.com>
2024-09-25 16:38:32 -0300

    Atualizado algumas questões lista 03

---
+++ JS03/app02.js
@@ -0,0 +1,15 @@
+let valor1 = document.querySelector("#valor1")
+let valor2 = document.querySelector("#valor2")
+let botaoVerificar = document.querySelector("#verificar")
+
+botaoVerificar.onclick = function () {
+    console.log("Whow")
+    for(;valor2.value == 0;){
+        valor2.value = prompt("Digite um novo valor")
+        console.log(alert("Valor inválido"))
+    }
+    console.log(`Resultado da divisão = ${Number.parseInt(valor1.value) / Number.parseInt(valor2.value)}`)
+    let texto = `${valor1.value} / ${valor2.value} = ${Number.parseInt(valor1.value) / Number.parseInt(valor2.value)}`
+    document.querySelector("#res").innerHTML = texto
+
+}
